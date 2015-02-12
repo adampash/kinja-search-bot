7b9e6fb9d90ab11e694ce28e7093b78c864f05ec
Adam Pash <adam.pash@gmail.com>
2015-02-12 10:00:56 -0500

    Config adjustments

--- web.rb
+++ web.rb
@@ -4,11 +4,13 @@ require 'slack-notifier'
 require_relative './lib/search'
 
 SECRETS = YAML::load(IO.read('config/secrets.yml'))
+SLACK_TOKEN = SECRETS["slack"]["token"] || ENV["SLACK_TOKEN"]
+SLACK_WEBHOOK = SECRETS["slack"]["webhook"] || ENV["SLACK_WEBHOOK"]
 # SEARCH_API = SECRETS["kinja"]["url"]
-notifier = Slack::Notifier.new SECRETS["slack"]["url"]
+notifier = Slack::Notifier.new SLACK_WEBHOOK
 
 post '/search' do
-  unless params["token"] == SECRETS["slack"]["token"]
+  unless params["token"] == SLACK_TOKEN
     status 404
   else
     result = Search.new params["text"]
