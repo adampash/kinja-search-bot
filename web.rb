1372f7a09a974ea35876e9721f8e0d63ab2ce180
Adam Pash <adam.pash@gmail.com>
2015-02-12 09:56:25 -0500

    token check

--- web.rb
+++ web.rb
@@ -3,11 +3,16 @@ require 'json'
 require 'slack-notifier'
 require_relative './lib/search'
 
-# SECRETS = YAML::load(IO.read('config/secrets.yml'))
+SECRETS = YAML::load(IO.read('config/secrets.yml'))
 # SEARCH_API = SECRETS["kinja"]["url"]
+notifier = Slack::Notifier.new SECRETS["slack"]["url"]
 
 post '/search' do
-  result = Search.new params["text"]
-  {text: result["data"]["headline"]}.to_json
-  result["data"]["headline"]
+  unless params["token"] == SECRETS["slack"]["token"]
+    status 404
+  else
+    result = Search.new params["text"]
+    {text: result["data"]["headline"]}.to_json
+    result["data"]["headline"]
+  end
 end
